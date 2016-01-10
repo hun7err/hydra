de2bd64b2d0839002d06102c8f93016b78d9ecea
Krzysztof Marciniak <kmarciniak@apptension.com>
2016-01-10 18:02:31 +0100

    Added docker commands for 3PC actors

--- lib/hydra.ex
+++ lib/hydra.ex
@@ -1,10 +1,16 @@
 defmodule Hydra do
-  def init(relative_config_path) do
+  # cohort:
+  #   sudo docker run --net=hydra0 -i -t trenpixster/elixir /bin/bash -c '(export IP_ADDR=`ip a | tail -4 | head -1 | tr -s " " | cut -d" " -f3 | cut -d/ -f1` && iex --name "cohort@$IP_ADDR" --cookie test)'
+  # coordinator:
+  #   iex --name "coordinator@172.18.0.1" --cookie test
+
+  defp get_servers(relative_config_path) do
     full_path = File.cwd! |> Path.join(relative_config_path)
-    %{"servers" => servers} = YamlElixir.read_from_file full_path
+    %{"servers" => servers,
+      "instances_per_node" => instances_per_node} = YamlElixir.read_from_file full_path
     servers
   end
 
-  def deploy(script) do
+  def deploy(script, config_path \\ "config.yml") do
   end
 end
